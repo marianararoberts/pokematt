0b9ddd4fe28775fa6f3e8a549878a2e20ae468f6
Your Name <you@example.com>
2022-06-11 19:53:47 -0400

    beginning collisions

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = 960;
-canvas.height = 540;
+canvas.width = 1024;
+canvas.height = 576;
+
+const collisionsMap = []
+for (let i = 0; i < collisions.length; i += 70) {
+  collisionsMap.push(collisions.slice(i, 70 + i))
+}
+
+class Boundary {
+  static width = 48;
+  static height = 48;
+  constructor({position}) {
+    this.position = position;
+    this.width = 48;
+    this.height = 48;
+  }
+
+  draw() {
+    c.fillStyle = 'red';
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+const boundaries = []
+const offset = {
+  x: -735,
+  y: -650
+}
+
+collisionsMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 1025)
+      boundaries.push(
+        new Boundary({
+          position: {
+            x: j * Boundary.width + offset.x,
+            y: i * Boundary.height + offset.y
+          }
+        })
+      )
+  })
+})
 
 c.fillStyle = 'white';
 c.fillRect(0, 0, canvas.width, canvas.height);
@@ -24,10 +64,11 @@ class Sprite {
     }
 }
 
+
 const background = new Sprite({
     position: {
-        x: 0,
-        y: -1350
+        x: offset.x,
+        y: offset.y
     },
     image: image
 })
@@ -50,14 +91,17 @@ const keys = {
 function playerMove() {
     window.requestAnimationFrame(playerMove)
     background.draw()
+    boundaries.forEach(boundary => {
+      boundary.draw();
+    })
     c.drawImage(
         playerImage,
         0,
         0,
         playerImage.width / 4,
         playerImage.height,
-        canvas.width / 2.71 - playerImage.width / 4 / 2,
-        canvas.height / 1.8 - playerImage.height / 2,
+        canvas.width / 2 - playerImage.width / 4 / 2,
+        canvas.height / 2 - playerImage.height / 2,
         playerImage.width / 4,
         playerImage.height
     )
